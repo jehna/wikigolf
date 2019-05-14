3c20f2868af2c35d058ea3a2f4dde8e8e5298cda
Jesse Luoto <jesse.luoto@gmail.com>
2019-05-14 22:44:14 +0300

    Fix CSV parsing errors

--- convert_page_to_csv.sh
+++ convert_page_to_csv.sh
@@ -2,4 +2,6 @@ FILE=fiwiki-latest-page.sql
 # head -n 100 $FILE | # For debugging
 cat $FILE |
 sed $'s/^INSERT INTO `page` VALUES (//; s/),(/\\\n/g' |
-awk -F ',' 'NR > 52 { print $1","$3 }'
+awk -F ',' 'NR > 51 && $2 == 0 { print $0 }' |
+awk -F $'\'' $'{ print $1"\'"$2"\'" }' |
+sed 's/,0,/,/'
--- convert_pagelinks_to_csv.sh
+++ convert_pagelinks_to_csv.sh
@@ -1,5 +1,6 @@
 FILE=fiwiki-latest-pagelinks.sql
-# head -n 50 $FILE | # For debugging
+#head -n 50 $FILE | # For debugging
 cat $FILE |
 sed $'s/^INSERT INTO `pagelinks` VALUES (//; s/),(/\\\n/g' |
-awk -F ',' 'NR > 40 && $2 == 0 { print $1","$3 }'
+awk -F ',' 'NR > 40 && $2 == 0 { print $0 }' |
+sed $'s/,0,/,/; s/\',0.*$/\'/'

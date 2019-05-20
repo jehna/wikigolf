bd85896b049a6dfc9c6619ed4ef25ae6f578f6db
Jesse Luoto <jesse.luoto@gmail.com>
2019-05-20 08:30:40 +0300

    Add results list

--- frontend/components/Wikigolf.tsx
+++ frontend/components/Wikigolf.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
-import { Atom, F } from '@grammarly/focal'
+import { Atom, F, reactiveList } from '@grammarly/focal'
 import PageSelector from './PageSelector'
 import { getShortestRoute } from '../model/api'
 
-const onSubmit = async (from: string, to: string) => {
-  const result = getShortestRoute(from, to)
-  console.log(result)
-}
+export default ({
+  from = Atom.create(''),
+  to = Atom.create(''),
+  results = Atom.create<string[]>([])
+}) => {
+  const onSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    results.set(await getShortestRoute(from.get(), to.get()))
+  }
 
-export default ({ from = Atom.create(''), to = Atom.create('') }) => {
   return (
     <div>
       <h1>Wikigolf</h1>
       <p>Find out the shortest path between two Wikipedia articles — easily!</p>
-      <form
-        onSubmit={e => {
-          e.preventDefault()
-          onSubmit(from.get(), to.get())
-        }}
-      >
+      <form onSubmit={onSubmit}>
         <label>
           From:
           <PageSelector onChange={value => from.set(value)} />
@@ -29,6 +28,16 @@ export default ({ from = Atom.create(''), to = Atom.create('') }) => {
         </label>
         <button type="submit">Search!</button>
       </form>
+      <h2>Results:</h2>
+      <F.ol>
+        {reactiveList(results, result => (
+          <li key={result}>
+            <a href={`https://fi.wikipedia.org/wiki/${result}`} target="_blank">
+              {result}
+            </a>
+          </li>
+        ))}
+      </F.ol>
     </div>
   )
 }

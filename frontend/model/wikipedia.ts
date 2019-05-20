0a3b0e6efb8cc2cd5c64967317e780260ee4395d
Jesse Luoto <jesse.luoto@gmail.com>
2019-05-20 08:44:13 +0300

    Remove redirects from autosuggestion results

--- frontend/model/wikipedia.ts
+++ frontend/model/wikipedia.ts
@@ -1,7 +1,7 @@
 import fetch from 'fetch-jsonp'
 
 const BASE_WIKI_SUGGEST_URL =
-  'https://fi.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&&namespace=0&limit=10&suggest=false&search='
+  'https://fi.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&&namespace=0&limit=10&suggest=false&redirects=resolve&search='
 
 export async function suggestWikiPage(input: string): Promise<string[]> {
   const req = await fetch(BASE_WIKI_SUGGEST_URL + encodeURIComponent(input))

9d38f070bb3ede50130db281a7320845af4399a1
Jesse Luoto <jesse.luoto@gmail.com>
2019-05-20 23:13:45 +0300

    Fix 404 result handling in frontend

--- frontend/model/api.ts
+++ frontend/model/api.ts
@@ -2,6 +2,10 @@ export async function getShortestRoute(from: string, to: string) {
   const req = await fetch(
     `/api/shortest-route?from=${toWikiUrl(from)}&to=${toWikiUrl(to)}`
   )
+  if (!req.ok) {
+    throw new Error('No path found 😩')
+  }
+
   const response = (await req.json()) as string[]
   return response
 }

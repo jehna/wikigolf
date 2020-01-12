1e09e165e936497c4e7c16c7cdd6fe6694e4f120
Jesse Luoto <jesse.luoto@gmail.com>
2020-01-12 21:59:35 +0200

    Add uploading to bigquery via node.js client

--- converter/load_to_bigquery.sh
+++ converter/load_to_bigquery.sh
@@ -2,6 +2,5 @@ set -e
 
 LANG=$1
 
-#node ./converter/download-and-convert.js $LANG &&
-bq load --field_delimiter tab "wikigolf:wikigolf_$LANG.pages" "./pages_$LANG.tsv" ./pages_schema.json &&
-bq load --field_delimiter tab "wikigolf:wikigolf_$LANG.pagelinkgs" "./pagelinks_$LANG.tsv" ./pagelinks_schema.json
+node ./converter/download-and-convert.js $LANG &&
+yarn ts-node ./converter/upload.ts $LANG
---
+++ converter/upload.ts
@@ -0,0 +1,40 @@
+import { atob } from '../backend/util'
+import { BigQuery } from '@google-cloud/bigquery'
+import dotenv from 'dotenv'
+import path from 'path'
+dotenv.config()
+
+const bigqueryClient = new BigQuery({
+  projectId: process.env.GCLOUD_PROJECT_ID,
+  credentials: {
+    private_key: atob(process.env.GCLOUD_PRIVATE_KEY_BASE64!),
+    client_email: process.env.GCLOUD_SERVICE_EMAIL
+  }
+})
+
+const upload = async (lang, table: 'pages' | 'pagelinks') => {
+  console.log(`Uploading ${table}_${lang}.tsv...`)
+
+  const filename = path.join(__dirname, '..', `${table}_${lang}.tsv`)
+  const [job] = await bigqueryClient
+    .dataset(`wikigolf_${lang}`)
+    .table(table)
+    .load(filename, { fieldDelimiter: '\t' })
+
+  console.log(`Uploading ${table}_${lang}.tsv completed, ${job.id}`)
+
+  // Check the job's status for errors
+  const errors = job.status?.errors ?? []
+  if (errors.length > 0) {
+    throw errors
+  }
+}
+
+const LANG = process.argv[2]
+
+if (!['fi', 'en'].includes(LANG))
+  throw new Error(`Language ${LANG} not supported!`)
+
+console.log(`Uploading ${LANG} wikipedia pages and pagelinks to BigQuery...`)
+upload(LANG, 'pages')
+upload(LANG, 'pagelinks')
